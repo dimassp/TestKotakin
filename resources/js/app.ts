0f03355e744970df98f9ab11212b455e8adde635
dimasteltics <dimas@teltics.com>
2025-05-22 00:59:49 +0700

    checkpoint primevue datatables

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,6 +5,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
+import PrimeVue from 'primevue/config';
 import { initializeTheme } from './composables/useAppearance';
 
 // Extend ImportMeta interface for Vite...
@@ -29,6 +30,9 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: 'none'
+            })
             .mount(el);
     },
     progress: {
